1329d390d3c9c0c2653deee6b4a8db648c57e0f7
Joseph Goodman <jgood1133@gmail.com>
2021-07-03 17:14:09 -0400

    🧪 - col, row, and nxn_grid methods all have passing unit tests

--- test/unit.py
+++ test/unit.py
@@ -1,11 +1,148 @@
 unit_tests = {}
 
 # dependencies
-import os, sys
+import os, sys, random
 
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # adds project dir to places it looks for the modules
 sys.path.append(BASE_PATH)
 
+from lib.board import Board
+
+# grid.return_row_of_index() always returns the correct row
+def test1(done):
+    # create two different boards to test on
+    big_board = Board()
+    little_board = Board(n=2)
+    why = Board(n=4)
+
+    boards = [big_board, little_board, why]
+
+    # its kind of hard to tell if a test is consistant when using random numbers so this test will be run 50 times
+    for i in range(50):
+        for board in boards:
+            # get the boards width and area
+            board_width = board.width
+
+            # change the boards elements so they are not all 0's 
+            # this way each row can be uniquely identified 
+            for i in range(board_width):
+                board.grid[i + (board_width*i)] = 1
+
+            # list of all rows
+            rows = []
+            for i in range(board_width):
+                rows.append(board.grid[ board_width*i : (board_width*i) + board_width ])
+
+            # choose a random row and then choose a random index in that row
+            row_index = random.randint(0,board_width-1)
+            expected_outcome = rows[row_index]
+
+            cell_index = random.randint(row_index*board_width,(row_index*board_width)+board_width-1)
+            
+            outcome = board.return_row_of_index(cell_index)
+
+            msg = "outcome:\n" + str(outcome) + "\nwas not equal to expected outcome:\n" + str(expected_outcome) + "\nwhen cell_index = " + str(cell_index) + "\nand row_index = " + str(row_index) + "\nthis happened during iteration: " + str(i+1)
+            assert outcome == expected_outcome, msg
+
+    # if no asserts failed the test is a pass
+    done("grid.return_row_of_index() always returns the correct row")
+unit_tests["grid.return_row_of_index() always returns the correct row"] = test1
+
+# grid.return_col_of_index() always returns the correct column
+def test2(done):
+    # create two different boards to test on
+    big_board = Board()
+    little_board = Board(n=2)
+    why = Board(n=4) 
+
+    boards = [big_board, little_board, why]
+
+    # its kind of hard to tell if a test is consistant when using random numbers so this test will be run 50 times
+    for i in range(50):
+        for board in boards:
+            # get the boards width and area
+            board_width = board.width
+            num_of_cells = board_width * board_width
+
+            # change the boards elements so they are not all 0's 
+            # this way each column can be uniquely identified 
+            for i in range(board_width):
+                board.grid[i + (board_width*i)] = 1
+
+            # list of all columns
+            columns = []
+            for i in range(board_width):
+                # TODO this method of getting the columns from a grid might be more 
+                # efficient than the way I do it in the actual board.py file... so check that later
+                columns.append(board.grid[i:num_of_cells:board_width]) 
+
+            # choose a random column and then choose a random index in that column
+            column_index = random.randint(0,board_width-1)
+            expected_outcome = columns[column_index]
+
+            cell_index = random.randrange(column_index,num_of_cells,board_width)
+            
+            outcome = board.return_col_of_index(cell_index)
+
+            msg = "outcome:\n" + str(outcome) + "\nwas not equal to expected outcome:\n" + str(expected_outcome) + "\nwhen cell_index = " + str(cell_index) + "\nand column_index = " + str(column_index) + "\nthis happened during iteration: " + str(i+1)
+            assert outcome == expected_outcome, msg
+
+    # if no asserts failed the test is a pass
+    done("grid.return_col_of_index() always returns the correct column")
+unit_tests["grid.return_col_of_index() always returns the correct column"] = test2
+
+# grid.return_nxn_grid_of_index always returns the correct sub_grid
+def test3(done):
+    # create two different boards to test on
+    big_board = Board()
+    little_board = Board(n=2)
+    why = Board(n=4)
+
+    boards = [big_board, little_board, why]
+
+    # its kind of hard to tell if a test is consistant when using random numbers so this test will be run 50 times
+    for i in range(50):
+        for board in boards:
+            # get the boards width and area
+            board_width = board.width
+            num_of_cells = board_width * board_width
+
+            # change the boards elements so they are not all 0's 
+            # this way each sub_grid can be uniquely identified 
+            for i in range(num_of_cells):
+                board.grid[i] = random.randint(0,2)
+
+            # list of all sub_grids
+            # TODO this method of getting the sub_grids from a grid might be more 
+            # efficient than the way I do it in the actual board.py file... so check that later
+            sub_grids = []
+            for i in range(board_width):
+                sub_grid = []
+                for x in range(board.n):
+                    # only ok becuase this is unit test
+                    # haha the next line has 188 chars in it 
+                    sub_grid += board.grid[ ((board_width*x) + ( (i%board.n) * (board.n) ) ) + ((i//board.n)*(board_width*board.n)) : ((board_width*x) + ( (i%board.n) * (board.n) )) + ((i//board.n)*(board_width*board.n))+board.n]
+                sub_grids.append(sub_grid)
+
+            # choose a random sub_grid and then choose a random index in that subgrid
+            sub_grid_index = random.randint(0,board_width-1)
+            expected_outcome = sub_grids[sub_grid_index]
+
+            up_down_pos = (sub_grid_index//board.n)*(board_width*board.n)
+            l_r_pos = (sub_grid_index%board.n)*(board.n)
+            cell_index = random.randrange(0,board.n) + (random.randrange(0,board.n) * board_width)
+            cell_index += up_down_pos + l_r_pos
+            
+            outcome = board.return_nxn_grid_of_index(cell_index)
+
+            msg = "outcome:\n" + str(outcome) + "\nwas not equal to expected outcome:\n" + str(expected_outcome) + "\nwhen cell_index = " + str(cell_index) + "\nand sub_grid_index = " + str(sub_grid_index) + "\nthis happened during iteration: " + str(i+1)
+            assert outcome == expected_outcome, msg
+
+    # if no asserts failed the test is a pass
+    done("grid.return_nxn_grid_of_index always returns the correct sub_grid")
+unit_tests["grid.return_nxn_grid_of_index always returns the correct sub_grid"] = test3
+
+'''
 # example tests
 def one_plus_one_is_two(done):
     outcome = 1+1
@@ -20,3 +157,4 @@ def one_plus_one_is_three(done):
     assert outcome == desired_outcome, "1+1 was not equal to three"
     done("one plus one is equal to three")
 unit_tests["one plus one is equal to three"] = one_plus_one_is_three
+'''
